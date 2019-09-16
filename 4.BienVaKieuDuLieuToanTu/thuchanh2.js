98a76af754a34211bdf10fbb0d8a92b354030da7
NguyenThanhSang-1 <sangskay00@gmail.com>
2019-09-16 10:58:15 +0700

    thuchanh2

---
+++ 4.BienVaKieuDuLieuToanTu/thuchanh2.js
@@ -0,0 +1,8 @@
+let width = 10;
+let height = 20;
+let inputWidth = prompt("Enter Width");
+let inputHeight = prompt("Enter Height");
+width = parseInt(inputWidth);
+height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area ís: " + area);
